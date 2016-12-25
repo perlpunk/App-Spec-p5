0cb8b84a1efd32853ba623af92530a4145021373
Tina Müller <cpan2@tinita.de>
2016-12-25 16:49:19 +0100

    pod for App::Spec::Run::Response

--- lib/App/Spec/Run/Response.pm
+++ lib/App/Spec/Run/Response.pm
@@ -1,3 +1,4 @@
+# ABSTRACT: Response class for App::Spec::Run
 use strict;
 use warnings;
 package App::Spec::Run::Response;
@@ -75,3 +76,71 @@ sub print_output {
 }
 
 1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+App::Spec::Run::Response - Response class for App::Spec::Run
+
+=head1 METHODS
+
+=over 4
+
+=item add_output
+
+If you pass it a string, it will create a L<App::Spec::Run::Output>.
+
+    $res->add_output("string\n", "string2\n");
+    my $output = App::Spec::Run::Output->new(
+        content => "string\n",
+    );
+    $res->add_output($output);
+
+=item add_error
+
+Like C<add_output>, but the created Output object will have an attribute
+C<error> set to 1.
+
+    $res->add_error("string\n", "string2\n");
+    my $output = App::Spec::Run::Output->new(
+        error => 1,
+        content => "string\n",
+    );
+    $res->add_error($output);
+
+=item print_output
+
+    $res->print_output(@out);
+
+=back
+
+=head1 ATTRIBUTES
+
+=over 4
+
+=item buffered
+
+If true, output should be buffered until print_output is called.
+
+=item exit
+
+The exit code
+
+=item outputs
+
+Holds an array of L<App::Spec::Run::Output> objects.
+
+=item finished
+
+Set to 1 after print_output has been called.
+
+=item halted
+
+If set to 1, no further processing should be done.
+
+=back
+
+=cut
