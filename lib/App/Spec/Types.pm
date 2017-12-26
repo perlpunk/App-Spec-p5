54f48584d851dd309d56d656d71827eb6865a679
dakkar <dakkar@thenautilus.net>
2017-12-26 15:25:01 +0000

    more types: some App::Spec::* class types

--- lib/App/Spec/Types.pm
+++ lib/App/Spec/Types.pm
@@ -5,11 +5,16 @@ package App::Spec::Types;
 use Type::Library -base,
     -declare => qw(
                       AppSpec
+                      SpecOption SpecParameter SpecSubcommand
               );
 use Type::Utils -all;
 use Types::Standard -types;
 use namespace::clean;
 
 class_type AppSpec, { class => 'App::Spec' };
+
+class_type SpecOption, { class => 'App::Spec::Option' };
+class_type SpecParameter, { class => 'App::Spec::Parameter' };
+class_type SpecSubcommand, { class => 'App::Spec::Subcommand' };
 
 1;
