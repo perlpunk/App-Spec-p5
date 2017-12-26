dceb1c1bacfa19fe03a5c516cd7f870f623fefb1
dakkar <dakkar@thenautilus.net>
2017-12-26 14:59:29 +0000

    type constraints in ::Output

--- lib/App/Spec/Run/Output.pm
+++ lib/App/Spec/Run/Output.pm
@@ -4,11 +4,12 @@ use warnings;
 package App::Spec::Run::Output;
 
 our $VERSION = '0.000'; # VERSION
+use Types::Standard qw/Bool/;
 
 use Moo;
 
 has type => ( is => 'rw', default => 'plain' );
-has error => ( is => 'rw' );
+has error => ( is => 'rw', isa => Bool, default => 0 );
 has content => ( is => 'rw' );
 
 1;
