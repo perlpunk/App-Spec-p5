c017656b0275bbb01540ca97b346e5a22630842b
Tina Müller <cpan2@tinita.de>
2019-04-22 21:38:31 +0200

    Fix bug (undefined array reference)

--- lib/App/Spec/Completion/Zsh.pm
+++ lib/App/Spec/Completion/Zsh.pm
@@ -87,7 +87,7 @@ sub completion_commands {
         level => $level,
         count => $level,
         functions => $functions,
-        previous => $args{previous},
+        previous => $previous,
     );
 
     if ($param_args) {
@@ -102,7 +102,7 @@ sub completion_commands {
             options => $options,
             level => $level,
             functions => $functions,
-            previous => $args{previous},
+            previous => $previous,
         );
         $arguments .= "$opt \\\n";
     }
